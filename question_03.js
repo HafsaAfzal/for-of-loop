3473b7957cde1eda04c397b91c13ed49eb01924d
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 12:56:14 +0500

    complete code added .code through error because objects are not iterable objects and for of loop iterates over iterable objects

---
+++ question_03.js
@@ -0,0 +1,10 @@
+// Create an object called `person` with properties like `name`, `age`, and `country`.
+// Use a `for...of` loop to iterate over the object and log each property value.
+const person={
+    name:"Hafsa",
+    age:20,
+    country:"Pakistan"
+}
+for (element of person){
+    console.log(element)
+}
