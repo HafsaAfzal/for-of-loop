3aeacce0f725b5f30017c39b10028a19e587dd1f
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 12:48:19 +0500

    complete and correct code added

---
+++ question_02.js
@@ -0,0 +1,6 @@
+// Create a string called `message` with some text. Use a `for...of` loop to iterate over
+// the characters of the string and log each character.
+let message="Hello! Hafsa";
+for ( str of message){
+    console.log(str);
+}
