99a8b7c5deb0fda5fba67e51053a80574b54a7f9
HafsaAfzal <hafsaafzal380@gmail.com>
2023-08-11 12:46:04 +0500

    complete and correct code added

---
+++ question_01.js
@@ -0,0 +1,6 @@
+// Create an array called `fruits` with some fruit names. Use a `for...of` loop to iterate
+// over the array and log each fruit name.
+let fruits=["apple","mango","banana","apricot"];
+for (val of fruits){
+    console.log(val);
+}
